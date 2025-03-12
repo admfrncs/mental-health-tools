0d27f61000135cc120b23c345d2b84ae037ae434
admfrncs <129465220+admfrncs@users.noreply.github.com>
2025-03-11 20:12:25 -0600

    jdjdjd

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,5 @@
-// src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma; // Store Prisma instance globally in dev mode
-}
+const prisma = new PrismaClient();
 
 export default prisma;
