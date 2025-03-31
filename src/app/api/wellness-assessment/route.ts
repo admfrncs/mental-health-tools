bb69ff341773a3dc9cd04fa26834e2b091386861
admfrncs <129465220+admfrncs@users.noreply.github.com>
2025-03-30 21:56:40 -0600

    lkdjsflsdjf

--- src/app/api/wellness-assessment/route.ts
+++ src/app/api/wellness-assessment/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { wellnessQuestions, calculateWellnessSectionScores } from "src/lib/wellnessQuestions";
+import { sectionDisplayNames , calculateSectionScores } from "src/lib/wellnessQuestions";
 
 export async function POST(req: Request) {
   try {
