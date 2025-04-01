24edef31fa40d89d2b3aa9ef4d579da79d937c05
admfrncs <129465220+admfrncs@users.noreply.github.com>
2025-04-01 11:50:49 -0600

    bleh 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           alt="Mental Health Tool Shop Logo" 
           width={300} // Doubled size 
           height={300} 
-          className="mx-auto" // Center logo and add spacing below
+          className="mb-2" // Center logo and add spacing below
           priority 
         />
 
